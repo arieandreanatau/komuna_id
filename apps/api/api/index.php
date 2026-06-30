146168c4c194269bf9234eb341ac738498eac476
Arie Andreana Taufiq <arieandreanataufiq1@gmail.com>
2026-06-30 09:01:08 +0700

    fix: putenv VIEW_COMPILED_PATH before bootstrap + useStoragePath

--- apps/api/api/index.php
+++ apps/api/api/index.php
@@ -7,30 +7,14 @@ define('LARAVEL_START', microtime(true));
 require __DIR__ . '/../vendor/autoload.php';
 
 $tmpStorage = '/tmp/storage';
-$dirs = [
-    $tmpStorage,
-    $tmpStorage . '/framework/cache/data',
-    $tmpStorage . '/framework/sessions',
-    $tmpStorage . '/framework/views',
-    $tmpStorage . '/logs',
-];
-
-foreach ($dirs as $dir) {
-    if (!is_dir($dir)) {
-        @mkdir($dir, 0755, true);
-    }
+foreach ([$tmpStorage, $tmpStorage.'/framework/cache/data', $tmpStorage.'/framework/sessions', $tmpStorage.'/framework/views', $tmpStorage.'/logs'] as $dir) {
+    if (!is_dir($dir)) @mkdir($dir, 0755, true);
 }
 
-putenv('VIEW_COMPILED_PATH=' . $tmpStorage . '/framework/views');
-putenv('SESSION_DRIVER=array');
-putenv('CACHE_STORE=array');
-putenv('LOG_CHANNEL=stderr');
+putenv("VIEW_COMPILED_PATH=$tmpStorage/framework/views");
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $app->useStoragePath($tmpStorage);
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle($request = Request::capture());
-$response->send();
-$kernel->terminate($request, $response);
+$app->handleRequest(Request::capture());
